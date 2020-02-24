e5a3417dcc17d59953059097c8661166664626b2
Brett Bedevian <brettbedevian@gmail.com>
2020-02-24 19:06:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(sentence)
   final = []
   sent_array = sentence.split(/ /)
   sent_array.each do |sent_array|
-    final.push(#{sent_array.reverse})
+    final << sent_array.reverse
     return final.join 
 end 
